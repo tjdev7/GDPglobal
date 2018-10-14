85df5b667b2a4a8e12db2671f34c56dcbfd47260
Toughee <MJ1menez@protonmail.com>
2018-10-14 18:03:29 -0500

    Finished developing application

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Map</h1>
-        <h4><strong>The World&#39;s financial data at your fingertips.</strong> Shows consumer data around the world</h4>  
+        <h1>GDPglobal</h1>
+        <h4><strong>The World&#39;s financial data at your fingertips.</strong> Shows financial data from every nation around the world</h4>  
         
+        <h5>Click on the circle to see data from each nation</h5>
         <p>About</p>
         <p>Github</p>
         
