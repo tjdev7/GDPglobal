96df2652e82ef14bd9bc433e7da108684fdd0627
Toughee <MJ1menez@protonmail.com>
2018-11-03 16:25:57 -0500

    PR #5

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ class App extends Component {
     return (
       <div className="App">
         <h1>GDPglobal</h1>
-        <h4> Shows financial data from every nation around the world</h4>  
+        <h4>Shows financial data from every nation around the world</h4>  
         <h5>Click on any circle inside the map to see data from each nation</h5>
-
       </div>
     );
   }
