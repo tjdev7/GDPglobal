8ff6597fa0d740b25498d34fc49b643b81501344
Toughee <MJ1menez@protonmail.com>
2020-06-17 17:24:58 -0500

    Updates

---
+++ precache-manifest.2a0dc0d0e06d674c610fc8a66ba4b804.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957d30e3dcb7583d84bac9c95606b258",
+    "url": "/GDPglobal/index.html"
+  },
+  {
+    "revision": "413f2f2476ff7f1c395d",
+    "url": "/GDPglobal/static/css/main.eb26a23d.chunk.css"
+  },
+  {
+    "revision": "e5c319799b5d9825ac0e",
+    "url": "/GDPglobal/static/js/2.4c3349d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GDPglobal/static/js/2.4c3349d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "413f2f2476ff7f1c395d",
+    "url": "/GDPglobal/static/js/main.df0c8e91.chunk.js"
+  },
+  {
+    "revision": "f9e22d9793d27fc64d1d",
+    "url": "/GDPglobal/static/js/runtime-main.a1807056.js"
+  }
+]);
