e3557ba313ec9b5f72ba3b2772162f885861455a
Toughee <MJ1menez@protonmail.com>
2020-06-18 12:04:38 -0500

    Updates

---
+++ precache-manifest.0dbc6c4a8c6143508d7b039c9e895366.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b948fce729da4b35bf779191d354760a",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "05545ac17efa70cae127",
+    "url": "/app/static/css/main.eb26a23d.chunk.css"
+  },
+  {
+    "revision": "e5c319799b5d9825ac0e",
+    "url": "/app/static/js/2.4c3349d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app/static/js/2.4c3349d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05545ac17efa70cae127",
+    "url": "/app/static/js/main.a1d28f58.chunk.js"
+  },
+  {
+    "revision": "e3f97e956d5002b2199a",
+    "url": "/app/static/js/runtime-main.33d9f303.js"
+  }
+]);
