71b40b4b591e4b49f70bc47e3e7740478460f444
Toughee <Jimernez.io@gmail.com>
2021-06-29 22:11:25 -0500

    GDPglobal/027/Dependencies_Update_012 - Updated U/I for application - Updated dependencies and committed audit fixes for node modules

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class map_data extends Component {
   render() {
     return (
-      <div className="side_Tab">
+      <section className="side_Tab">
         <h1>GDPglobal</h1>
         <h4>Shows financial data from every nation around the world</h4>  
         <h5>Click on any circle inside the map to see data from each nation</h5>
@@ -13,7 +13,7 @@ class map_data extends Component {
         <p>Source: <a href ={"https://wits.worldbank.org/countrystats.aspx?lang=en"}> World Bank</a>
         </p>
 
-      </div>
+      </section>
     );
   }
 }
