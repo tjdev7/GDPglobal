9b29aa44d344a12dbda2057c400e0d476569c9ba
Toughee <Jimernez.io@gmail.com>
2021-08-05 16:29:50 -0500

    GDPglobal/029/README_Update_01 - Updated README to improve user interaction with app - Updated documentation for future app contributors - Updated node modules inside app - Committed minor code refactoring inside app

--- README.md
+++ README.md
@@ -5,8 +5,6 @@
 
 SPA data visualizer created in React. Shows financial data for over 200 nations around the world. Have the financial information for almost every country on planet earth with a few swipes and clicks on your screen. App currently does not work in the Internet Explorer Browser.
 
-![GDPglobal App screenshot](src/img/screenshot_app.JPG)
-
 ###  Current data it provides:
 
 - Exports per year in thousands of $US dollars
@@ -22,3 +20,12 @@ SPA data visualizer created in React. Shows financial data for over 200 nations 
 ### Source
 
 - [World Bank/WITS](https://wits.worldbank.org/countrystats.aspx?lang=en)
+
+
+#### How to use
+
+
+1. Clone repo into desired directory ```https://github.com/Toughee/GDPglobal.git``` or ```gh repo clone Toughee/GDPglobal```
+2. Run ```npm install``` to install all app node modules and dependencies
+3. Run ```yarn start``` to run app on http://localhost:3000/
+
--- src/js/mapRender.js
+++ src/js/mapRender.js
@@ -1,28 +1,39 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2VyZy0xMjEyIiwiYSI6ImNqbHpndDE1ajBwNGszdm53azIwajR1bjUifQ.-1NJchgY5Hx3kYqCJalPxw';
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoic2VyZy0xMjEyIiwiYSI6ImNqbHpndDE1ajBwNGszdm53azIwajR1bjUifQ.-1NJchgY5Hx3kYqCJalPxw";
 var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/serg-1212/cjn67c54h11882rlpojtf4h65',
-    center: [-75.50, 42], // starting position
-    zoom: 3 // starting zoom
+  container: "map", // container id
+  style: "mapbox://styles/serg-1212/cjn67c54h11882rlpojtf4h65",
+  center: [-75.5, 42], // starting position
+  zoom: 3 // starting zoom
 });
 
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
 
-map.on('click', function(e) {
-    var features = map.queryRenderedFeatures(e.point, {
-        layers: ['gdp-all']
-    });
+map.on("click", function(e) {
+  var features = map.queryRenderedFeatures(e.point, {
+    layers: ["gdp-all"]
+  });
 
-    if (!features.length) {
-        return;
-    }
+  if (!features.length) {
+    return;
+  }
 
-    var feature = features[0];
+  var feature = features[0];
 
-    var popup = new mapboxgl.Popup({ offset: [0, -15] })
-        .setLngLat(feature.geometry.coordinates)
-        .setHTML('<h3>' + feature.properties.name + '</h3><p>' + feature.properties.expdta + '</p><p>' + feature.properties.gdpdta + '</p><p>' + feature.properties.impdta + '</p>')
-        .setLngLat(feature.geometry.coordinates)
-        .addTo(map);
+  var popup = new mapboxgl.Popup({ offset: [0, -15] })
+    .setLngLat(feature.geometry.coordinates)
+    .setHTML(
+      "<h3>" +
+        feature.properties.name +
+        "</h3><p>" +
+        feature.properties.expdta +
+        "</p><p>" +
+        feature.properties.gdpdta +
+        "</p><p>" +
+        feature.properties.impdta +
+        "</p>"
+    )
+    .setLngLat(feature.geometry.coordinates)
+    .addTo(map);
 });
