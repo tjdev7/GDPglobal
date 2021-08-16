4efe8f79e998b37d520ca6f6c14b6baad71efca8
Toughee <Jimernez.io@gmail.com>
2021-08-16 09:54:43 -0500

    GDPglobal/030/Testing_Scripts_and_Documentation - Modified tests scripts and added Lighthouse audit options for app - Edited README for better project contributions - Updated dependencies and committed audit fixes

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "../src/App";
+import App from "./App";
 
-it("renders sidebar for data visualizer with no errors", () => {
+it("Renders app sidebar for data visualizer with no errors or issues", () => {
   const div = document.createElement("div");
+  // const appSidebar
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- tests/App.test.js
+++ tests/App.test.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "../src/App";
 
-it("renders without crashing", () => {
+it("Renders app sidebar for data visualizer with no errors or issues", () => {
   const div = document.createElement("div");
+  // const appSidebar
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
